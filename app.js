37c0630830774903589c699db691f7d53e532c7a
matheusferfreitas <matheus.freitas@getrak.com.br>
2024-06-12 15:20:32 -0300

    acrescentando o console teste

--- app.js
+++ app.js
@@ -2,8 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("eviando para o GitHub")
 console.log("TESTE NO GITHUB")
-
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao banco de dados")
-console.log("eviando para o GitHub")
 console.log("teste")
